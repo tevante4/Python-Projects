997e900fe917fe1e08ddf6d86361113a0514c01b
Tevante Swanston <tevswany@gmail.com>
2022-05-19 14:18:20 -0700

    Abstraction Assignment

---
+++ Abstraction.py
@@ -0,0 +1,6 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/tevanteswanston/Documents/Abstraction .py ==========
+Your purchase amount:  $400
+Your purchase amount of $400 exceeded your $100 limit
