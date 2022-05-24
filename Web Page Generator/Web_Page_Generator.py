6fe4c7762e4fb1a792ff6c7136cfe6e5f166ba27
Tevante Swanston <tevswany@gmail.com>
2022-05-24 00:27:48 -0700

    Updated "webbrowser.open_new_tab."

--- Web Page Generator/Web_Page_Generator.py
+++ Web Page Generator/Web_Page_Generator.py
@@ -9,7 +9,7 @@ import webbrowser
 def openInBrowser():
     url = "step310_web_page_generator_assignment.html"
     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-    webbrowser.get(chrome_path).open(url)
+    webbrowser.open_new_tab.
 
 
 # Function to create the html file (if it doesn't already exists) and overwrite its content with the htmlFileContent string below.
